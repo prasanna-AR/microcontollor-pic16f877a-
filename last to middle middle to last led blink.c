f8881770213e9d1fcbe4125871c5f11df5e54a39
prasanna-AR <115212325+prasanna-AR@users.noreply.github.com>
2022-10-09 10:00:49 -0200

    Add files via upload

---
+++ last to middle middle to last led blink.c
@@ -0,0 +1,59 @@
+/****************/
+/* Author : Prasanna */
+/* Date   : 28-09-2022 */
+/* Filename:last to middle and middle to last led blink */
+/* Description :last to middle and middle to last */
+/****************/
+#include<htc.h>
+#include<stdio.h>
+#define _XTAL_FREQ 20000000
+__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & BOREN_ON & LVP_OFF);
+
+void main()
+{
+	TRISB=0;//port b as output
+	while(1)
+	{
+		PORTB=0x80;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x40;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x20;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x10;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x10;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x20;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x40;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x80;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		}
+		
+} 
