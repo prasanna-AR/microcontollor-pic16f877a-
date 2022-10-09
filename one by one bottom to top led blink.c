305dc1f6736d3f9fe9d936dfa096f8f3adcfe8f5
prasanna-AR <115212325+prasanna-AR@users.noreply.github.com>
2022-10-09 10:02:56 -0200

    Add files via upload

---
+++ one by one bottom to top led blink.c
@@ -0,0 +1,59 @@
+/****************/
+/* Author : Prasanna */
+/* Date   : 28-09-2022 */
+/* Filename:one by one bottom to top led blink */
+/* Description :one by one bottom to top */
+/****************/
+#include<htc.h>
+#include<stdio.h>
+#define _XTAL_FREQ 20000000
+__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & BOREN_ON & LVP_OFF);
+
+void main ()
+{
+	TRISB=0;//port b as output
+	while(1)
+	{
+		PORTB=0x80;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x40;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x20;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x10;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x08;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0X04;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x02;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x01;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		}
+		
+}
