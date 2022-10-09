079e96fa9aa2b785521615ab348c2fd2cbe970fc
prasanna-AR <115212325+prasanna-AR@users.noreply.github.com>
2022-10-09 09:58:52 -0200

    Add files via upload

---
+++ frist to last and last to frist led blink.c
@@ -0,0 +1,111 @@
+/****************/
+/* Author : Prasanna */
+/* Date   : 28-09-2022 */
+/* Filename:first to last and last to first led blink */
+/* Description :first to last and last to first*/
+/****************/
+#include<htc.h>
+#include<stdio.h>
+#define _XTAL_FREQ 20000000
+__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & BOREN_ON & LVP_OFF);
+
+void main()
+{
+	TRISB=0;//port b as output
+	while(1)
+	{
+		PORTB=0x01;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x02;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x04;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x08;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x10;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x20;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x40;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+		PORTB=0x80;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+			
+		PORTB=0x80;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+			
+		PORTB=0x40;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+			
+		PORTB=0x20;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+			
+		PORTB=0x10;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+			
+		PORTB=0x08;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+			
+		PORTB=0x04;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+			
+		PORTB=0x02;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+			
+		PORTB=0x01;
+		__delay_ms(300);
+		PORTB=0x00;
+		__delay_ms(500);
+		
+	}
+	
+}	
+	
+	
+		
+			
